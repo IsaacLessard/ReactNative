0f435b0c7989e96dfd8ffc38b8ae1a83fe82c855
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-05-27 19:22:49 -0700

    :rocket: initial commit

---
+++ List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class List extends React.Component {
+
+  render() {
+
+    return (
+      <View>
+        <Text>Testing</Text>
+      </View>
+    )
+  }
+}
