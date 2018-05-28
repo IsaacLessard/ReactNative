e2b5eed6cd0e64f17b8c305c7263627f573c4d80
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-05-27 20:04:18 -0700

    :wrench: basic navigation

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import List from './List'
+import { View, Text, Button, TextInput } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test!</Text>
-        <List/>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Add a meal"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class DetailsScreen extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ""
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(text) {
+    this.setState({text})
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Insert ingredients here, seperated with commas!"
+          onChangeText={(text) => this.handleInput(text)}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
--- List.js
+++ List.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
 
 export default class List extends React.Component {
 
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
-
+    const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>Testing</Text>
-      </View>
-    )
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
   }
 }
